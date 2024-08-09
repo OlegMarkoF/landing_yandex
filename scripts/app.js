92d4ce0c3080470fcf2a137ba16c0c55ad2ac5e3
Oleg Markov <o.mark@mail.ru>
2024-08-09 17:36:57 +0200

    Тестовое задание для yandex

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,62 @@
-let slideIndex = 1;
-let cardIndex = 1;
-const indicators = Array.from(document.querySelectorAll(".indicator"));
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const cards = document.querySelectorAll(".split");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const nextCardBtn = document.querySelector(".next-card__btn");
+const prevCardBtn = document.querySelector(".prev-card__btn");
+const participantCount = document.querySelector(".participant-count");
+const active = document.querySelector(".active");
 
-let timer = setInterval(function(){
-  slideIndex++;
-  showSlides(slideIndex);
+const indicators = Array.from(document.querySelectorAll(".indicator"));
+let currentSlide = 0;
+let currentCard = 1;
+
+let timer = setInterval(function () {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide();
 }, 4000);
 
-showSlides(slideIndex);
-showCardsSlides(slideIndex);
+showCardsSlides(currentCard);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+function showSlide() {
+  slides.forEach((slide, index) => {
+    if (window.innerWidth >= 1024) {
+      if (index === currentSlide) {
+        slide.style.display = "flex";
+        slide.classList.add("active");
+      } else if (
+        index === currentSlide - 1 ||
+        (currentSlide === 0 && index === slides.length - 1)
+      ) {
+        slide.style.display = "flex";
+        slide.classList.remove("active");
+      } else if (
+        index === currentSlide + 1 ||
+        (currentSlide === slides.length - 1 && index === 0)
+      ) {
+        slide.style.display = "flex";
+        slide.classList.remove("active");
+      } else {
+        slide.style.display = "none";
+        slide.classList.remove("active");
+      }
+    } else if (window.innerWidth < 1024) {
+      if (index === currentSlide) {
+        slide.style.display = "flex";
+        slide.classList.add("active");
+      } else {
+        slide.style.display = "none";
+        slide.classList.remove("active");
+      }
+    }
+    participantCount.textContent = currentSlide + 1 + " / " + slides.length;
+  });
 }
-
-function nextCard(n) {
-  showCardsSlides((cardIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+showSlide();
 
 function activateIndicator(index) {
   indicators.forEach((indicator, i) => {
@@ -28,41 +64,65 @@ function activateIndicator(index) {
   });
 }
 
-function showSlides(n) {
-  const slides = document.getElementsByClassName(
-    "slideshow-container__participants"
-  );
-
-  const participantCount = document.querySelector(".participant-count");
-
-  if (n > slides.length) {
-    slideIndex = 1;
+function updateButtons() {
+  console.log(currentCard)
+  if (currentCard === cards.length) {
+    prevCardBtn.disabled = true;
+  } else {
+    prevCardBtn.disabled = false;
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+  if (currentCard <= 1) {
+    nextCardBtn.disabled = true;
+  } else {
+    nextCardBtn.disabled = false;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  participantCount.textContent = slideIndex + " / " + slides.length;
-  slides[slideIndex - 1].style.display = "flex";
-
 }
 
-function showCardsSlides(n) {
-  const cards = document.getElementsByClassName("split");
-  const nextBtn = document.querySelector("next-card__btn");
-  const prevBtn = document.querySelector("prev-card__btn");
-
-  if (n > cards.length) {
-    cardIndex = 1;
-  }
-  if (n < 1) {
-    cardIndex = cards.length;
-  }
-  for (i = 0; i < cards.length; i++) {
-    cards[i].style.display = "none";
-  }
-  cards[cardIndex - 1].style.display = "block";
-  activateIndicator(cardIndex - 1);
+function showCardsSlides() {
+  cards.forEach((slide, index) => {
+    if (window.innerWidth < 1330) {
+      if (index === currentCard - 1) {
+        slide.style.display = "flex";
+      } else {
+        slide.style.display = "none";
+      }
+    } else {
+      slide.style.display = "flex";
+    }
+  });
+  activateIndicator(currentCard - 1);
 }
+showCardsSlides();
+
+prevCardBtn.addEventListener("click", () => {
+  currentCard++;
+  updateButtons();
+  showCardsSlides()
+});
+nextCardBtn.addEventListener("click", () => {
+  currentCard--;
+  updateButtons();
+  showCardsSlides()
+});
+
+prevBtn.addEventListener("click", () => {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide();
+});
+
+nextBtn.addEventListener("click", () => {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide();
+});
+
+window.addEventListener("resize", () => {
+  showSlide;
+  showCardsSlides();
+  updateButtons;
+});
